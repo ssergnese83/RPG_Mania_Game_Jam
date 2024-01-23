9531129c72f691b42417d9a153e9db0c83f270e9
ssergnese83 <ssergnese83@gmail.com>
2024-01-23 14:01:35 -0500

    added classes/*.cpp

--- build.bat
+++ build.bat
@@ -4,4 +4,4 @@
 
 @REM g++ src/main.cpp src/0_startup_screen/startup_screen.cpp src/1_title_screen/title_screen.cpp -Wl,--subsystem,windows -mwindows -o game -I include -I classes -lraylib -lopengl32 -lgdi32 -lwinmm
 
-g++ src/main.cpp src/0_startup_screen/startup_screen.cpp src/1_title_screen/title_screen.cpp src/2_menu_screen/menu_screen.cpp src/3_overworld_screen/overworld_screen.cpp src/4_battle_screen/battle_screen.cpp -o game -I include -I classes -lraylib -lopengl32 -lgdi32 -lwinmm
+g++ classes/*.cpp src/main.cpp src/0_startup_screen/startup_screen.cpp src/1_title_screen/title_screen.cpp src/2_menu_screen/menu_screen.cpp src/3_overworld_screen/overworld_screen.cpp src/4_battle_screen/battle_screen.cpp -o game -I include -I classes -lraylib -lopengl32 -lgdi32 -lwinmm
