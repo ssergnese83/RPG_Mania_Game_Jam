46cb33c69bd0c91c0c6013191f8b72bdf8f86b40
Chris <chris.canelea@mail.utoronto.ca>
2024-01-23 13:58:13 -0500

    player not compiling

--- src/3_overworld_screen/overworld_screen.cpp
+++ src/3_overworld_screen/overworld_screen.cpp
@@ -1,4 +1,5 @@
 #include "globals.hpp"
+#include "character.hpp"
 #include <raylib.h>
 #include <stdio.h>
 
@@ -16,6 +17,7 @@ void overworld_screen(void) {
         overworld_screen_loop(overworld_screen_vars);
     }
 
+    // next screen logic
     previous_screen = current_screen;
     current_screen = next_screen;
 
@@ -26,14 +28,16 @@ void overworld_screen(void) {
 void overworld_screen_loop(void* arg_) {
     OverworldScreenVars* overworld_screen_vars = (OverworldScreenVars*) arg_;
 
-    //processing
-    
-    
-    // drawing
+    Character player;
+
     BeginDrawing();
         ClearBackground(RAYWHITE);
         DrawText("OVERWORLD SCREEN", SCREEN_W/2 - 384, 104, 80, GREEN);
 
+        // player
+        DrawTexturePro(player.getSprite(0), {0,0,48,48}, player.getHitbox(), {0,0}, 0, WHITE);
+
+        // debug
         if (debug_mode) {
             draw_debug_stuff();
             if (end_loop == true) {
