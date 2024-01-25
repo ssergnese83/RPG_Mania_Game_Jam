51838ba3d48ab6a81116eafc6ace661055f3f525
ssergnese83 <ssergnese83@gmail.com>
2024-01-24 22:41:12 -0500

    WIP

--- src/2_menu_screen/menu_screen.cpp
+++ src/2_menu_screen/menu_screen.cpp
@@ -2,24 +2,29 @@
 #include <raylib.h>
 #include <stdio.h>
 
-#define RAYGUI_IMPLEMENTATION
-#include <raygui.h>
+// #define RAYGUI_IMPLEMENTATION
+// #include <raygui.h>
 
 void menu_screen_loop(void* arg_);
 
 typedef struct MenuScreenVars {
     Vector2 mouse_position;
-    Rectangle play_button;
-    Rectangle bounds;
+    Rectangle continue_button;
+    Rectangle new_game_button;
+    Rectangle options_button;
+    Rectangle selection;
+    int menu_option;
 } MenuScreenVars;
 
 void menu_screen(void) {
-    GuiLoadStyleDefault();
-
     MenuScreenVars* menu_screen_vars = new MenuScreenVars;
+
     menu_screen_vars->mouse_position = {0.0,0.0};
-    menu_screen_vars->play_button = {20, 350, 700, 40};
-    menu_screen_vars->bounds = {50, 50, 500, 500};
+    menu_screen_vars->continue_button = {1100, 400, 700, 150};
+    menu_screen_vars->new_game_button = {1100, 600, 700, 150};
+    menu_screen_vars->options_button = {1100, 800, 700, 150};
+    menu_screen_vars->selection = {1125, 425, 750, 200};
+    menu_screen_vars->menu_option = 0;
 
     while (!end_loop) {
         menu_screen_loop(menu_screen_vars);
@@ -32,25 +37,41 @@ void menu_screen(void) {
 
 void menu_screen_loop(void* arg_) {
     MenuScreenVars* menu_screen_vars = (MenuScreenVars*) arg_;
-
+    
     menu_screen_vars->mouse_position = GetMousePosition();
 
-    int temp = GuiLabelButton(menu_screen_vars->bounds, "test");
+    if (IsKeyPressed(KEY_W)) {
+        // play a sound effect
+        menu_screen_vars->menu_option--;
+    } else if (IsKeyPressed(KEY_S)) {
+        // play a sound effect
+        menu_screen_vars->menu_option++;
+    }
+
+    if (menu_screen_vars->menu_option < 0) {
+        menu_screen_vars->menu_option = 0;
+    } else if (menu_screen_vars->menu_option > 2) {
+        menu_screen_vars->menu_option = 2;
+    }
 
     BeginDrawing();
         ClearBackground(RAYWHITE);
         DrawText("MENU SCREEN", SCREEN_W/2 - 384, 104, 80, GREEN);
-        DrawText("AND file selection (WIP) :D", 20, 300, 35, RED);
-        if(CheckCollisionPointRec(menu_screen_vars->mouse_position, menu_screen_vars->play_button)) {
-            DrawRectangleRec(menu_screen_vars->play_button, GREEN);
-            if (IsMouseButtonPressed(MOUSE_LEFT_BUTTON)) {
-                end_loop = true;
-                return;
-            }
-        } else {
-            DrawRectangleRec(menu_screen_vars->play_button, YELLOW);
+        
+        if (menu_screen_vars->menu_option == 0) {
+            DrawRectangleRec(menu_screen_vars->continue_button, BLUE);
+        } else if (menu_screen_vars->menu_option == 1) {
+            DrawRectangleRec(menu_screen_vars->new_game_button, BLUE);
+        } else if (menu_screen_vars->menu_option == 2) {
+            DrawRectangleRec(menu_screen_vars->options_button, BLUE);
         }
-        DrawText("Click here to start crude gameplay!", 20, 350, 35, RED);
+
+        DrawRectangleLinesEx(menu_screen_vars->continue_button, 2, BLACK);
+        DrawRectangleLinesEx(menu_screen_vars->new_game_button, 2, BLACK);
+        DrawRectangleLinesEx(menu_screen_vars->options_button, 2, BLACK);
+
+        DrawText("Continue", 1225, 450, 75, RED);
+        
 
         if (debug_mode) {
             draw_debug_stuff();
